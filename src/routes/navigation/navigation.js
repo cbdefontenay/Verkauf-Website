223c7db485043aded027d2cf122807a42a3e1b01
Cyprien de Fontenay <cbdefontenay@gmail.com>
2023-01-02 09:03:31 +0100

    Adding Context

--- src/routes/navigation/navigation.js
+++ src/routes/navigation/navigation.js
@@ -1,10 +1,12 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { UserContext } from "../../contexts/user.context";
 import './navigation.styles.scss';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
 
 const Navigation = () => {
+    const { currentUser } = useContext(UserContext);
     return(
         <Fragment>
             <div className="navigation">
